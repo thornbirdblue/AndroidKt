d67c5f7949b44ef8f43aadf571fc2d8e7e3ad8b3
liuchangjian <liuchangjian@bbktel.com>
2020-10-09 19:19:01 +0800

    HelloKt APP
    Change-Id: I0557b32f282890e8456f5b8c537be9e9e3876a3c

---
+++ src/com/example/android/hellokt/HelloKt.kt
@@ -0,0 +1,14 @@
+package com.example.android.hellokt
+
+//import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.app.Activity
+
+class HelloKt : Activity() {
+	    override fun onCreate(savedInstanceState: Bundle?) {
+	        super.onCreate(savedInstanceState)
+	        setContentView(R.layout.hello_activity)
+	    }
+}
+
+
